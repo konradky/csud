36dbd597186ff9070b8afe5b95ce078298da6d77
Konrad Kazmirek <Konrad.Kazmirek@sabre.com>
2019-05-29 20:47:13 +0000

    sample failing test

---
+++ src/test/groovy/kk/sudoku/SomeTest.groovy
@@ -0,0 +1,12 @@
+package kk.sudoku
+
+import spock.lang.Specification
+
+class SomeTest extends Specification
+{
+    def "verify if surefire works"() {
+        expect:
+        1 == 2 + 3
+
+    }
+}
